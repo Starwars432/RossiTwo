a2b058bd1fd31bb2c0f0928bdaa3bd1ece3cda5d
Starwars432 <manifestillusions@gmail.com>
2025-04-21 19:31:04 -0400

    23

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,14 +7,7 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  server: {
-    fs: {
-      allow: ['..'],
-    },
-  },
+      input: './index.html'
+    }
+  }
 });
