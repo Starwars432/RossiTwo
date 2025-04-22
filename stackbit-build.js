bb6dc12fd1c773070e4063092310634ffbc22f42
Starwars432 <manifestillusions@gmail.com>
2025-04-22 00:12:45 -0400

    34

--- stackbit-build.js
+++ stackbit-build.js
@@ -21,7 +21,7 @@ if (!fs.existsSync(distDir)) {
 // Run build command
 try {
   console.log('Running build process...');
-  execSync('npx vite build', { 
+  execSync('npm run build', { 
     stdio: 'inherit',
     env: { 
       ...process.env,
